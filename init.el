066d06303047fabdfa86ac27a9e14ac11ee8a7a7
Sergey Petrunin <halfstrik@gmail.com>
2019-03-09 00:58:49 -0500

    Switched to use-package config

--- init.el
+++ init.el
@@ -1,29 +1,12 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
- '(custom-enabled-themes (quote (tango-dark)))
- '(ido-enable-flex-matching t)
- '(ns-pop-up-frames nil)
- '(package-archives
-   (quote
-	(("gnu" . "https://elpa.gnu.org/packages/")
-	 ("melpa-stable" . "http://stable.melpa.org/packages/"))))
- '(package-selected-packages (quote (projectile magit)))
- '(scroll-bar-mode (quote right))
- '(tab-width 4)
- '(tool-bar-mode nil))
+;; Common
+(load-theme 'tango-dark t)
+;; Backup directory
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+;; Window & Frames
+(setq ns-pop-up-frames nil)
+(tool-bar-mode -1)
+(scroll-bar-mode 'right)
+(setq-default tab-width 4)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -32,19 +15,49 @@
  ;; If there is more than one, they won't work right.
  '(term-color-blue ((t (:background "blue2" :foreground "dark cyan")))))
 
+;; Package
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (auto-package-update-maybe))
+
 ;; Term
-(eval-after-load "term"
-  '(define-key term-raw-map (kbd "C-c C-y") 'term-paste))
+(use-package term
+  :bind ("C-c C-y" . term-paste)
+  ;;:config
+  ;;(custom-set-variables
+  ;;   '(term-color-blue ((t (:background "blue2" :foreground "dark cyan")))))
+  )
 
 ;; Ido mode
-(require 'ido)
-(ido-mode t)
+(use-package ido
+  :config
+  (setq ido-enable-flex-matching t)
+  (ido-mode t))
 
 ;; Magit
-(global-set-key (kbd "C-c g") 'magit-status)
+(use-package magit
+  :bind ("C-c g" . magit-status))
 
 ;; Projectile
-(require 'projectile)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-(projectile-mode +1)
+(use-package projectile
+  :bind-keymap (("s-p" . projectile-command-map)
+				("C-c p" . projectile-command-map))
+  :config
+  (projectile-mode t))
