c1227dfa85eb784e745ab58510ece65ab43b9297
Sergey Petrunin <halfstrik@gmail.com>
2019-03-11 00:16:25 -0400

    Update custom.el to work with flycheck

--- custom.el
+++ custom.el
@@ -1,3 +1,7 @@
+;;; custom.el --- Custom config file
+;;; Commentary:
+;;; Code:
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -8,10 +12,14 @@
 	(exec-path-from-shell flycheck projectile magit auto-package-update use-package)))
  '(safe-local-variable-values
    (quote
-	((flycheck-gcc-include-path (list (stringp)))))))
+	((eval setq flycheck-gcc-include-path
+		   (list
+			(expand-file-name "~/Projects/anysolo-db.project/anysolo-db/")))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(term-color-blue ((t (:background "blue2" :foreground "dark cyan")))))
+
+;;; custom.el ends here
